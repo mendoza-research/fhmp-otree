39fc9fb8597c7ef03b73062f8e91f9877fa973e1
Ye Joo Park <subwaymatch@gmail.com>
2019-04-04 14:23:35 -0700

    Add comments in pages.py

--- fhmp_practice_rounds/pages.py
+++ fhmp_practice_rounds/pages.py
@@ -1,10 +1,39 @@
 from ._builtin import Page, WaitPage
 
+# Each page that a player sees is defined in this file
+# Each class in this file represents a page
+
+# Each page can inherit either a "Page" class or a "WaitPage" class
+# If a page inherits a "WaitPage" class, the page is simply a page to wait until
+# all players arrive
+# Example 1: class BeginWaitPage(Waitpage) is a page class that inherits "WaitPage" class
+# This page is not shown to players and is used to wait until all players are on the same page (no pun intended!)
+
+# Example 2: class SellerChoice(Page) is a page that inherits "Page" class
+# This page is shown to players
+
+# For detailed documentation regarding pages, please refer to oTree's documentation page below
+# https://otree.readthedocs.io/en/latest/pages.html
+
 # This is a transition page that is not shown to the end-user
-#
 class BeginWaitPage(WaitPage):
+	# This method is called once all players arrive at this page.
 	def after_all_players_arrive(self):
+		# init_assets() is a method defined in models.py
+		# The init_assets() method does two things as described below
+		# 1. It generates a random probability for each asset being a "high quality" asset
+		# 2. It determines the true asset value based on the probablity generated from Step 1
 		self.group.init_assets()
+		
+		# set_players_budgets() is a method defined in models.py
+		# It sets the remaining budget for each player
+		
+		# For a seller, the budget will always be 0
+		
+		# For a buyer, two cases exist. 
+		# If first round, the budget will be set to initial endowment amount (300 multipled by number of rounds)
+		# As an example, if a player has 20 rounds, the initial endowment will be set to 6,000 for the first round
+		# If later than first round, budget will be decreased by the amount used in purchasing an asset (with winning bid)
 		self.group.set_players_budgets()
 
 
