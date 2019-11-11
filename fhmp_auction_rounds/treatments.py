f598e869e4ac579b821757475c350d44981194b2
Ye Joo Park <subwaymatch@gmail.com>
2019-11-12 07:55:49 +0900

    Change field types in exit survey, merge changes from Dr. Mendoza

--- fhmp_auction_rounds/treatments.py
+++ fhmp_auction_rounds/treatments.py
@@ -3,8 +3,9 @@ import os
 # A separate file to set treatments
 
 # For local testing
-os.environ['CAN_CHOOSE_PRECISION'] = 'True'
-os.environ['IS_GRADE_PASS_FAIL'] = 'True'
+# Uncomment the lines below to manually set treatment environment variables
+# os.environ['CAN_CHOOSE_PRECISION'] = 'True'
+# os.environ['IS_GRADE_PASS_FAIL'] = 'False'
 
 
 class Treatments:
@@ -12,9 +13,9 @@ class Treatments:
     # if is_choice is False, all sellers are forced to see the more precise option
     # if is_choice is False, reporting cost will be 0
     can_choose_precision = os.getenv(
-        'CAN_CHOOSE_PRECISION') == 'True' if 'CAN_CHOOSE_PRECISION' in os.environ else True
+        'CAN_CHOOSE_PRECISION').strip().lower() == 'true' if 'CAN_CHOOSE_PRECISION' in os.environ else True
 
     # is_pass_fail indicates whether the fact checker grade should be Pass/Fail or A/B/C/F
     # A,B,C => Pass, D => Fail
     is_grade_pass_fail = os.getenv(
-        'IS_GRADE_PASS_FAIL') == 'True' if 'IS_GRADE_PASS_FAIL' in os.environ else True
+        'IS_GRADE_PASS_FAIL').strip().lower() == 'true' if 'IS_GRADE_PASS_FAIL' in os.environ else True
--- fhmp_exit_survey/models.py
+++ fhmp_exit_survey/models.py
@@ -26,15 +26,13 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    gpa = models.StringField(label='What is your GPA? ')
-    gender = models.StringField(label='Gender: ')
+    gpa = models.FloatField(label='What is your GPA?')
+    gender = models.StringField(label='Gender', choices=['Male', 'Female'])
     is_english_first_language = models.BooleanField(
         label='Is English your first language?')
     major = models.StringField(label='What is your major?')
-    years_in_college = models.StringField(
+    years_in_college = models.FloatField(
         label='How many years have you been in college?')
     age = models.IntegerField(label='What is your age?')
     comments = models.StringField(
-        label='Do you have any questions or comments about the study? If so, please specify below. ')
-
-    pass
+        label='Do you have any questions or comments about the study? If so, please specify below', initial="")
